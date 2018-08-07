94df1942e2f43a4edc9944d2475a9a7ced9c2efa
Akash271193 <akash_killer1993@hotmail.com>
2018-08-07 11:49:06 -0400

    Adding webiste template

---
+++ js/portfolio.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	getPortfolio();
+	
+
+});
+function processPortfolio(data){
+	var result = $.parseJSON(data);
+	var html = [];
+	console.log(result);
+	if(Array.isArray(result)){
+		var id = 0;
+		result.forEach(function(project) {    
+		//========================
+		//PORTFOLIO SETUP
+		//=======================
+			html.push('<li><a href="' + project.videoUrl + '">');
+			html.push('<img src="' + project.videoThumbnailUrl + '" ');
+			html.push('alt="' + project.projectName  + '"></a>');
+			html.push('<h3><a href="' + project.videoUrl+ '">' + project.projectName + '</h3></li>')
+
+		$('#shotsByPlayerId').html(html.join(''));
+		});	
+	}
+	else if(result == null){
+	}
+	else{
+		
+	}
+};
+
+function getPortfolio(){
+	jQuery.support.cors = true;
+	$.get("http://portfolio.com/apis/getProjects",processPortfolio);
+};
+
